decf286cbeffbf955a2ca6ed0010a517157cb5fe
elijahcbrooks <elijahcbrooks@gmail.com>
2021-05-12 01:50:15 -0500

    JTN-078 feat: creation of form, server adaptesr, and data retrieval

--- src/components/pages/LandingPage.js
+++ src/components/pages/LandingPage.js
@@ -1,15 +1,34 @@
-import SideBar from '../SideBar';
-import '../../style/landing-page.css'
+import '../../style/landing-page.css';
+import { useState } from 'react';
+import { Form } from 'react-bootstrap';
+import RestaurantService from '../../adapters/restaurantService';
 
 const LandingPage = () => {
+
+  const [ searchText, setSearchText ] = useState('');
+
+  const searchForContent = (e) => {
+    if(e) e.preventDefault();
+    if(searchText){
+      RestaurantService.getRestaurants(searchText)
+      .then(response => response.json())
+      .then(console.log)
+    }
+  }
+
   return(
     <div id = "landing-page">
       <div id = "title-holder">
         <h1 id="title-name"> SCRUMPTIOUS </h1>
         <div id="search-bar">
-          <input className="input-text" placeholder="Enter restaurant name">
-          </input>
-          <div id="search-button">
+          <Form onSubmit={searchForContent}>
+            <input
+              className="input-text"
+              onChange={e => setSearchText(e.target.value)}
+              placeholder="Enter restaurant name"
+            ></input>
+          </Form>
+          <div id="search-button" onClick={searchForContent}>
             <i className="fas fa-angle-right"></i>
           </div>
         </div>
