7224eb12db4ecdf0888bf063faf797cbf048cece
Jim Brower <jimrabrower@gmail.com>
2021-05-24 16:14:22 -0700

    removed searchreducer

--- src/reducers/rootReducer.js
+++ src/reducers/rootReducer.js
@@ -1,10 +1,8 @@
 import {combineReducers} from 'redux';
-import searchReducer from './searchReducer';
 import authReducer from './authReducer';
 import errorReducer from './errorReducer';
 
 const rootReducer = combineReducers({
-   search: searchReducer,
    auth: authReducer,
    error: errorReducer,
 });
